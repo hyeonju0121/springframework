65e179d1ac5ef43e2cd152a6aaaaf4ef414cc803
yu hyun ju <a01085249773@gmail.com>
2024-04-22 14:31:55 +0900

    Annotation 을 사용한 의존성 주입

--- src/main/java/com/mycompany/springframework/service/Ch12Service3.java
+++ src/main/java/com/mycompany/springframework/service/Ch12Service3.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Service // 기본적으로 클래스의 첫자를 소문자로한 이름으로 관리 (ch12Service3)
+// 관리객체는 이름으로 관리가 된다.
+// 기본적으로 클래스의 첫자를 소문자로한 이름으로 관리 (ch12Service3)
+@Service
 @Slf4j
 public class Ch12Service3 {
 	public Ch12Service3() {
--- src/main/java/com/mycompany/springframework/service/Ch12Service4.java
+++ src/main/java/com/mycompany/springframework/service/Ch12Service4.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Service("service4") // service4 이름으로 관리
+//service4 이름으로 관리
+@Service("service4") 
 @Slf4j
 public class Ch12Service4 {
 	public Ch12Service4() {
